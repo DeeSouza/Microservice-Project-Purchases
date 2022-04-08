350847db9ec4d8fc5fb34694dc086e92828c43be
Diego Souza <dee-souza@hotmail.com>
2022-04-08 10:12:07 -0300

    configured and setup microservices kafka service

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import * as path from 'path';
+
 import { CustomersService } from '../services/customers.service';
 import { ProductsService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
@@ -11,11 +12,13 @@ import { DatabaseModule } from '../database/database.module';
 import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchaseResolver } from './graphql/resolvers/purchases.resolver';
+import { MessagingModule } from '../messaging/messaging.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
